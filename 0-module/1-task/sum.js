75e7c8de57c6bd92d4ce7e565d9ebba222d10cf4
Evgenij Labuzov <silksofthesoul@yandex.ru>
2020-02-07 03:33:34 +0300

    add solution for 1-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,16 @@
+/* eslint-disable */
+const type = val => typeof val;
+const isExist = arg => !(arg === undefined || arg === null || isNaN(arg));
+const isNumber = (val) => {
+  if (!isExist(val)) return false;
+  if (type(val) !== 'number') return false;
+  if (Object.getPrototypeOf(val).constructor.name !== 'Number') return false;
+  return true;
+};
+
 function sum(a, b) {
-  /* ваш код */
+  if(!isNumber(a)||!isNumber(b))throw TypeError();
+  return a + b;
 }
 
 module.exports = sum;
