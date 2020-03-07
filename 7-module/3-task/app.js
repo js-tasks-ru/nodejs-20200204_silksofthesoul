5ae8bba96853173612465a7db68b42f210579582
javascript-ru <47786167+jsru@users.noreply.github.com>
2020-03-07 21:16:57 +0100

    Added solution for task: Работа с пользователем, сессии

--- 7-module/3-task/app.js
+++ 7-module/3-task/app.js
@@ -32,10 +32,11 @@ app.use(async (ctx, next) => {
 app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
-
+    await Session.create({token, user, lastVisit: new Date()});
+    
     return token;
   };
-
+  
   return next();
 });
 
@@ -44,7 +45,18 @@ const router = new Router({prefix: '/api'});
 router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
   if (!header) return next();
-
+  
+  const token = header.split(' ')[1];
+  if (!token) return next();
+  
+  const session = await Session.findOne({token}).populate('user');
+  if (!session) {
+    ctx.throw(401, 'Неверный аутентификационный токен');
+  }
+  session.lastVisit = new Date();
+  await session.save();
+  
+  ctx.user = session.user;
   return next();
 });
 
@@ -53,7 +65,7 @@ router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
 
